38beccf38072c8bf0edb159f609542f7a7e5b783
bgt <bgt@sci.fi>
2014-10-07 22:25:18 +0300

    #1 "GHRDM can draw the commit log of a project in GitHub" completed

--- src/GitHubConnection.py
+++ src/GitHubConnection.py
@@ -2,9 +2,15 @@ from github import Github
 
 class GitHubConnection(object):
     def __init__(self, user, repo):
-        self.gitHubUser = user
-        self.gitHubRepo = repo
+        self.gitHubUserName = user
+        self.gitHubRepoName = repo
         self.github = Github()
+        #self.repo = self.github.get_repo(self.gitHubUserName+"/"+self.gitHubRepoName)
+        self.repo = self.github.get_repo(user+"/"+repo)
         
     def getCommits(self):
-        return ""
+        commits = self.repo.get_commits()
+        reva = ""
+        for commit in commits:
+            reva = reva + commit.commit.message+"\n"
+        return reva
--- src/test/CoreTests.py
+++ src/test/CoreTests.py
@@ -11,7 +11,7 @@ class Test(unittest.TestCase):
 
     #GHRDM can draw the commit log of a project in GitHub
     def testGitHubConnection(self):
-        connection = GitHubConnection(user="bgt", repo="GitHubResearchDataMiner")
+        connection = GitHubConnection(user="tomibgt", repo="GitHubResearchDataMiner")
         commits = connection.getCommits()
         validFlag = commits.count('\n') > 2
         self.assertTrue(validFlag, "Cannot draw commit logs from GitHub")
