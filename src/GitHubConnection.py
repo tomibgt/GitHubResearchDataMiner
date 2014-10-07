ccfd35d37dc23074c4a3d18304d1945e6e08dcdd
bgt <bgt@sci.fi>
2014-10-07 20:12:09 +0300

    Added the pygithub to the project as a library as adviced at http://stackoverflow.com/questions/12519327/how-to-define-an-external-source-folder-in-pydevproject

--- src/GitHubConnection.py
+++ src/GitHubConnection.py
@@ -1,7 +1,10 @@
+from github import Github
+
 class GitHubConnection(object):
     def __init__(self, user, repo):
         self.gitHubUser = user
         self.gitHubRepo = repo
+        self.github = Github()
         
     def getCommits(self):
         return ""
