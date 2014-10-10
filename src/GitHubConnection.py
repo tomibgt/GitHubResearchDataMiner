be5cda58e06581e9aff33ad6a58d03c8c13a282e
bgt <bgt@sci.fi>
2014-10-10 15:11:21 +0300

    This should take care of issue #4, but there are no tests yet.

--- src/GitHubConnection.py
+++ src/GitHubConnection.py
@@ -1,14 +1,26 @@
+import datetime
+import time
+import GitHubResearchDataMiner
 from github import Github
+
 
 class GitHubConnection(object):
     def __init__(self, user, repo):
         self.gitHubUserName = user
         self.gitHubRepoName = repo
-        self.github = Github()
-        #self.repo = self.github.get_repo(self.gitHubUserName+"/"+self.gitHubRepoName)
+        self.github = Github(GitHubResearchDataMiner.config.get('authentication', 'ghusername'), GitHubResearchDataMiner.config.get('authentication', 'ghpassword'))
         self.repo = self.github.get_repo(user+"/"+repo)
+        self.requestRateTimer = datetime.datetime.now().microsecond
+        
+    #This method is used to limit the rate of requests sent to GitHub
+    def __choke(self):
+        delta = datetime.datetime.now().microsecond-self.requestRateTimer
+        if delta < 80000:
+            time.sleep(delta/1000000)
+        self.requestRateTimer = datetime.datetime.now().microsecond
         
     def getCommitMessages(self):
+        self.__choke()
         commits = self.repo.get_commits()
         reva = ""
         for commit in commits:
@@ -19,6 +31,7 @@ class GitHubConnection(object):
     def getCsv(self):
         filepath = 'output.csv'
         fileh = open(filepath, 'w')
+        self.__choke()
         commits = self.repo.get_commits()
         for commit in commits:
             fileh.write(self.getCsvLineFromCommit(commit)+'\n')
