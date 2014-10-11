5afa978c3e2f92a2a35582173bab7f91a8b57cd9
bgt <bgt@sci.fi>
2014-10-11 11:04:13 +0300

    Another timer test.

--- src/test/CodeArchitectureTest.py
+++ src/test/CodeArchitectureTest.py
@@ -3,8 +3,11 @@ Created on Oct 10, 2014
 
 @author: bgt
 '''
+
+import time
 import unittest
 import GitHubResearchDataMiner
+import HelperFunctions
 
 class CodeArchitectureTest(unittest.TestCase):
 
@@ -13,6 +16,13 @@ class CodeArchitectureTest(unittest.TestCase):
         self.assertIsNotNone(GitHubResearchDataMiner.config.get('authentication', 'ghusername'), "Configuration failed")
         self.assertIsNotNone(GitHubResearchDataMiner.config.get('authentication', 'ghpassword'), "Configuration failed")
 
+    def testMilliTimeStamp(self):
+        alpha = HelperFunctions.millitimestamp()
+        time.sleep(1)
+        beta = HelperFunctions.millitimestamp()
+        self.assertGreater(beta-alpha, 900, "millitimestamp counts units smaller than milliseconds")
+        self.assertLess(beta-alpha, 1500, "millitimestamp counts units greater than milliseconds")
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testConfiguration']
     unittest.main()
